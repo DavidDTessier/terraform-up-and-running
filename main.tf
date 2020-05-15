2dfa46c1127f98d778dd404c538a927563e9b975
David Tessier <dtessier@Davids-MacBook-Pro.local>
2020-05-15 07:41:13 -0400

    updates

--- main.tf
+++ main.tf
@@ -3,12 +3,42 @@ provider "aws" {
     region = "ca-central-1"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
 // create micro ec2 instance
 resource "aws_instance" "my-instance" {
     ami = "ami-07744b6c7178930b5" // Amazon Machine Image ID of an Ubuntu 18.04 AMI  in Canada Central
     instance_type = "t2.micro"    // EC2 Instance to run
-
+    vpc_security_group_ids = [aws_security_group.my-instance-sec-grp.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
     tags = {
         Name = "terraform-example"
     }
 }
+
+//security group
+resource "aws_security_group" "my-instance-sec-grp" {
+    name = "terraform-example-sec-grp"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] // Allow from any IP
+    }
+  
+}
+
+output "public_ip" {
+  value = aws_instance.my-instance.public_ip
+  description = "The public IP address of the web server"
+}
+
