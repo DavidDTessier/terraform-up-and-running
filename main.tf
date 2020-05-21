692e33995fc93fd5dadb9a73185ae9754540a1ed
David Tessier <dtessier@Davids-MacBook-Pro.local>
2020-05-21 08:10:41 -0400

    add updates

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "server_port" {
 }
 
 // create micro ec2 instance
-resource "aws_instance" "my-instance" {
+/*resource "aws_instance" "my-instance" {
     ami = "ami-07744b6c7178930b5" // Amazon Machine Image ID of an Ubuntu 18.04 AMI  in Canada Central
     instance_type = "t2.micro"    // EC2 Instance to run
     vpc_security_group_ids = [aws_security_group.my-instance-sec-grp.id]
@@ -23,6 +23,55 @@ resource "aws_instance" "my-instance" {
         Name = "terraform-example"
     }
 }
+*/
+
+resource "aws_launch_configuration" "launch-confg-example" {
+    image_id = "ami-07744b6c7178930b5" // Amazon Machine Image ID of an Ubuntu 18.04 AMI  in Canada Central
+    instance_type = "t2.micro"    // EC2 Instance to run
+    security_group_ids = [aws_security_group.my-instance-sec-grp.id]
+    
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    # Required when using a launch configuration with an 
+    # auto scaling group
+    # https://www.terraform.io/docs/providers/aws/r/launch_c onfiguration.html
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "asg-example" {
+  launch_configuration = aws_launch_configuration.launch-confg-example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.aws_subnet_ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+      key  = "Name"
+      value = "terraform-asg-example"
+      propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "name" {
+  
+}
+
+
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
 
 //security group
 resource "aws_security_group" "my-instance-sec-grp" {
